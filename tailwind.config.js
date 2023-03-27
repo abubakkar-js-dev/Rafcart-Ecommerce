2f872a95d59023cf957b834973b523e0449ce602
abubakkar-js-dev <mdabubakkars182@gmail.com>
2023-03-27 15:50:18 +0600

    updated all

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,22 @@ module.exports = {
     "./src/**/*.{html,css,js}",
   ],
   theme: {
-    extend: {},
+    container : {
+      center: true,
+      padding:{
+          DFAULT: '1rem',
+          sm: '2rem',
+          md: '3rem',
+          lg: '4rem',
+          xl: '5rem',
+         '2xl': '6rem'
+      },
+    },
+    extend: {
+      aspectRatio:{
+        '4/3' : '4 / 3',
+      },
+    },
   },
   plugins: [],
 }
